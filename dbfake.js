cc406cfb87fcc149718ef6e14b7e48f9247c62e5
muelFLEX <152731569+muelFLEX@users.noreply.github.com>
2023-12-03 01:37:53 -0300

    Add files via upload

---
+++ dbfake.js
@@ -0,0 +1,27 @@
+var db = {
+    dados: [
+        {
+            titulo: '',
+            descrição: '',
+            album: '',
+            Flag: '',
+        }, {
+            titulo: 'Europa',
+            descrição: '',
+            album: '',
+            Flag: ''
+        }, {
+            titulo: 'Oceania',
+            descrição: '',
+            album: '',
+            Flags: '',
+        },
+
+        {
+            titulo: 'Ásia',
+            descrição: '',
+            album: '',
+            Flag: '',
+        },
+    ]
+} 
